6fdf09a3b9fc17654408e9dd6b9e93edc3362bc1
amrinlong <programrin2020@gmail.com>
2022-04-06 18:57:22 -0700

    first commit

---
+++ popup.js
@@ -0,0 +1,10 @@
+const start = document.getElementById("work");
+const start = document.getElementById("break");
+const start = document.getElementById("cycle");
+
+document.getElementById("submit").addEventListener("click", startTimer)
+
+startTimer(setTimeout(function[])) {
+
+}
+
