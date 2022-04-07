de39c91aabd37fc7b161d881d6222101956a7594
amrinlong <programrin2020@gmail.com>
2022-04-06 19:02:53 -0700

    first reload of chrome extension

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
-const start = document.getElementById("work");
-const start = document.getElementById("break");
-const start = document.getElementById("cycle");
+const workStartButton = document.getElementById("work");
+const breakStartButton = document.getElementById("break");
+const cycleStartButton = document.getElementById("cycle");
 
 document.getElementById("submit").addEventListener("click", startTimer)
 
